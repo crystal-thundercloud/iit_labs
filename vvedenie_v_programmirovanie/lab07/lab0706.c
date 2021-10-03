2336d05512f4a4c6ce66c4683fd2db2673d881d2
crystal-thundercloud <91133063+crystal-thundercloud@users.noreply.github.com>
2021-10-03 15:22:16 +0300

    Update lab0706.c
    Найти решение системы линейных уравнений вида 
    A1·x + B1·y = C1, 
    A2·x + B2·y = C2

--- vvedenie_v_programmirovanie/lab07/lab0706.c
+++ vvedenie_v_programmirovanie/lab07/lab0706.c
@@ -3,3 +3,20 @@
 A1·x + B1·y = C1, 
 A2·x + B2·y = C2
 */
+
+#include <stdio.h>
+
+int main()
+{
+    float a1, b1, c1, a2, b2, c2, x, y;
+    
+    printf("Введите значения первого уравнения: ");
+    scanf("%g%g%g", &a1, &b1, &c1);
+    printf("Введите значения второго уравнения: ");
+    scanf("%g%g%g", &a2, &b2, &c2);
+    
+    x = (c1*b2-c2*b1)/((a1*b2)-(a2*b1));
+    y = (a1*c2-a2*c1)/((a1*b2)-(a2*b1));
+    
+    printf("x = %g\ny = %g", x, y);
+}
