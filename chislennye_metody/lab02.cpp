88ec46078e049f014fd1bd30586b0547d6d980ba
crystal-thundercloud <91133063+crystal-thundercloud@users.noreply.github.com>
2022-03-09 23:53:51 +0300

    Update lab02.cpp

--- chislennye_metody/lab02.cpp
+++ chislennye_metody/lab02.cpp
@@ -7,19 +7,41 @@ double mainfunc(double x) { //функция в виде (2x-3)^2-e^x
     return(pow(2*x-3, 2)-pow(M_E, x));
 }
 
-double iter(double x) {
-    return((pow(M_E,x/2)-3)/-2);
+void iter(double x, double e) { //простые итерации
+    int i=0;
+    double temp;
+    do {
+        temp=x;
+        x=(pow(M_E,x/2)-3)/-2;
+        i++;
+    }
+    while (abs(x-temp)>=e);
+    cout<<"\nМетод простых итераций:"<<endl;
+    cout<<"Корень x="<<x<<"\nНайден на итерации "<<i<<"\nПри точности "<<e<<endl;
 }
 
-/*double dych(double a, double b) {
-    return((a+b)/2);
-}*/
+void dych(double a, double b, double x, double e) { //дихотомия
+    int i=0;
+    double temp, tempa, tempb;
+    do {
+        temp = x;
+        x = (a+b)/2;
+        tempa = a;
+        tempb = b;
+        a = mainfunc(x)>0? x: tempa;
+        b = mainfunc(x)<0? x: tempb;
+        i++;
+    }
+    while (abs(x-temp)>=e);
+    cout<<"\nМетод дихотомии:"<<endl;
+    cout<<"Корень x="<<x<<"\nНайден на итерации "<<i<<"\nПри точности "<<e<<endl;
+}
 
 int main()
 {
 cout<<R"(Вариант 15. Исходное выражение:
 (2x-3)^2=e^x
-Введите границы отрезка (а, b при a<b) и точность e: )";
+Введите границы отрезка (а, b при a<b) и точность e (пример: 0 1 0.00000001): )";
 double a, b, e;
 cin>>a>>b>>e;
 while (a>=b) {
@@ -27,11 +49,9 @@ while (a>=b) {
     cin>>a>>b;
 }
 
-double x=0;
+double x=a;
+iter(x, e);
+dych(a, b, x, e);
 
-cout<<"\n"<<iter(x)<<"\n"<<mainfunc(x);
-
-
-
-
+return 0;
 }
