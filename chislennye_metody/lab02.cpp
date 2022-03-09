945be1561c993b3515f68090a6bb12cae6703168
crystal-thundercloud <91133063+crystal-thundercloud@users.noreply.github.com>
2022-03-09 03:50:01 +0300

    Update lab02.cpp

--- chislennye_metody/lab02.cpp
+++ chislennye_metody/lab02.cpp
@@ -3,22 +3,33 @@
 #include <math.h>
 using namespace std;
 
-double func(double x) { //функция в виде (2x-3)^2-e^x
-   return(pow(2*x-3, 2)-pow(M_E, x));
+double mainfunc(double x) { //функция в виде (2x-3)^2-e^x
+    return(pow(2*x-3, 2)-pow(M_E, x));
 }
 
-double iter(double a, double e) {
-    return 0;
+double iter(double x) {
+    return((pow(M_E,x/2)-3)/-2);
 }
+
+/*double dych(double a, double b) {
+    return((a+b)/2);
+}*/
 
 int main()
 {
 cout<<R"(Вариант 15. Исходное выражение:
-(2x-3)^2=e^x)";
+(2x-3)^2=e^x
+Введите границы отрезка (а, b при a<b) и точность e: )";
+double a, b, e;
+cin>>a>>b>>e;
+while (a>=b) {
+    cout<<"Неверные границы отрезка. Задайте a и b ещё раз.\n";
+    cin>>a>>b;
+}
 
 double x=0;
-double result = (pow(M_E,x/2)-3)/-2;
-cout<<"\n"<<result<<"\n"<<func(x);
+
+cout<<"\n"<<iter(x)<<"\n"<<mainfunc(x);
 
 
 
