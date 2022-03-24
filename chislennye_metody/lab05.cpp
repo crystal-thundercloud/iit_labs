c71b4c4f7663184caea4a54facae518c6b541727
crystal-thundercloud <91133063+crystal-thundercloud@users.noreply.github.com>
2022-03-24 12:24:14 +0300

    Create lab05.cpp

---
+++ chislennye_metody/lab05.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int n=100, a=1, b=2, c=1, d=10*30;
+    double u[n+1], v[n+1], x[n+1];
+    
+    u[1] = -c/b;
+    v[1] = d/b;
+    
+    for (int i=2; i<=n; i++) {
+        u[i] = -c/(a*u[i-1]+b);
+        v[i] = (d-a*v[i-1])/(a*u[i-1]+b);
+    }
+    
+    x[n] = (d-a*v[n-1])/(a*u[n-1]+b);
+    cout<<"x[100] = "<<x[n]<<endl;
+    
+    for (int i = n-1; i>0; i--) {
+        x[i] = u[i]*x[i+1]+v[i];
+        if (i>9) cout<<setw(3)<<"x["<<i<<"] = "<<x[i]<<endl;
+        else cout<<setw(4)<<"x["<<i<<"] = "<<x[i]<<endl;
+    }
+
+    return 0;
+}
