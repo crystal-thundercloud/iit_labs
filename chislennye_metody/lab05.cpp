2ac3a7a3fd3498e26a55fadc5d8f927facfc3dad
crystal-thundercloud <91133063+crystal-thundercloud@users.noreply.github.com>
2022-03-24 22:01:33 +0300

    Update lab05.cpp

--- chislennye_metody/lab05.cpp
+++ chislennye_metody/lab05.cpp
@@ -5,16 +5,21 @@ using namespace std;
 
 int main()
 {
-    int n=100, a=1, b=2, c=1, d=10*30;
+    cout<<"Метод прогонки: "<<endl;
+    const int n=100;
+    int a=1, b=2, c=1, d=10*30; //номер в списке - 30
     double u[n+1], v[n+1], x[n+1];
     
-    u[1] = -c/b;
+    u[1] = -(double)c/b; //-1/2, но считало как 0, так как c и b - int
     v[1] = d/b;
     
     for (int i=2; i<=n; i++) {
+        if (i==n) c=0;
         u[i] = -c/(a*u[i-1]+b);
         v[i] = (d-a*v[i-1])/(a*u[i-1]+b);
     }
+    
+    c=1;
     
     x[n] = (d-a*v[n-1])/(a*u[n-1]+b);
     cout<<"x[100] = "<<x[n]<<endl;
@@ -23,6 +28,7 @@ int main()
         x[i] = u[i]*x[i+1]+v[i];
         if (i>9) cout<<setw(3)<<"x["<<i<<"] = "<<x[i]<<endl;
         else cout<<setw(4)<<"x["<<i<<"] = "<<x[i]<<endl;
+        if (i==1) cout<<b*x[i]+c*x[i+1]-d;
     }
 
     return 0;
