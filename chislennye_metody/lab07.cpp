21c6b320dadb482f6a8efa28b5a1091be18f5039
crystal-thundercloud <91133063+crystal-thundercloud@users.noreply.github.com>
2022-04-02 22:49:20 +0300

    Update lab07.cpp

--- chislennye_metody/lab07.cpp
+++ chislennye_metody/lab07.cpp
@@ -15,18 +15,21 @@ int n;
 
 cout<<"Введите целое число n: "; //450
 cin>>n;
-    
-double a = 0, b = 120, h = (b-a)/n, f[n], x[n+1], fa, fb, sum;
-fa = (pow(a,4))/((4*pow(a,5))+2);
-fb = (pow(b,4))/((4*pow(b,5))+2);
-    
-for (int i=1; i<=n; i++) {
-    x[i] = a+i*h;
-    f[i] = (pow(x[i],4))/((4*pow(x[i],5))+2);
-    sum += f[i];
-}
-    
-cout<<setprecision(14)<<h*(0.5*(fa+fb)+sum)<<"\n"<<"Вычисленное значение: 1,23153029472451"<<endl;
 
+for (int i=0; i<5; i++) {    
+    double a = 0, b = 120, h = (b-a)/n, f[n], x[n], fa, fb, sum=0;
+    fa = (pow(a,4))/((4*pow(a,5))+2);
+    fb = (pow(b,4))/((4*pow(b,5))+2);
+        
+    for (int i=1; i<n; i++) {
+        x[i] = a+i*h;
+        f[i] = (pow(x[i],4))/((4*pow(x[i],5))+2);
+        sum += f[i];
+    }
+        
+    cout<<"n = "<<n<<"\nОтвет, полученный программой: "<<setprecision(15)<<h*(0.5*(fa+fb)+sum)
+    <<"\nВычисленное вручную значение: 1.23153029472451"<<endl;
+    n*=2;
+}
 return 0;
 }
